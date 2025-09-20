95658a6125dc61d233257129f5a9844231ef7f57
nnlgsakib <nlgarts@gmail.com>
2025-09-20 18:15:09 +0600

    addeed te embed

---
+++ embed.go
@@ -0,0 +1,20 @@
+package kubo
+
+import (
+	"embed"
+	"io/fs"
+	"log"
+	"net/http"
+)
+
+//go:embed build/*
+var embeddedFiles embed.FS
+
+// GetHandler returns an http.Handler to serve the embedded frontend build
+func GetHandler() http.Handler {
+	subFS, err := fs.Sub(embeddedFiles, "build")
+	if err != nil {
+		log.Fatalf("Failed to load embedded frontend: %v", err)
+	}
+	return http.FileServer(http.FS(subFS))
+}
